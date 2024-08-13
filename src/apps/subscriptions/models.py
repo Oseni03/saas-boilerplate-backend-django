15ece1227ea8b2c47ab1f036a303c29ad0854f95
Oseni03 <oseniayomide57@gmail.com>
2024-08-14 00:06:58 +0100

    feat: Sync User Subscription with the User Group

--- src/apps/subscriptions/models.py
+++ src/apps/subscriptions/models.py
@@ -45,10 +45,33 @@ class UserSubscription(models.Model):
 def user_sub_post_save(sender, instance, *args, **kwargs):
     user_sub_instance = instance
     user = user_sub_instance.user
-    print(user_sub_instance)
     subscription_obj = user_sub_instance.subscription
-    groups = subscription_obj.groups.all()
-    user.groups.set(groups)
+    groups_ids = []
+    if subscription_obj is not None:
+        groups = subscription_obj.groups.all()
+        groups_ids = groups.values_list("id", flat=True)
+    if not settings.ALLOW_CUSTOM_GROUP:
+        # If user does not belong to any other type of group 
+        # apart from subscription groups, then set the user group only to the 
+        # group of the newly subscribed subscription
+        user.groups.set(groups)
+    else:
+        # Set the user group to the combination of any other type of group the user might belong to 
+        # and the only to the newly subscribed subscription
+
+        # To get the list of all subscription except for the newly subscribed one
+        subs_qs = Subscription.objects.filter(active=True)
+        if subscription_obj is not None:
+            subs_qs = subs_qs.exclude(id=subscription_obj.id)
+        # To get the list of group ids for each of the subscription above
+        subs_groups = subs_qs.values_list("groups__id", flat=True)
+        subs_groups_set = set(subs_groups)
+        # groups_ids = groups.values_list("id", flat=True) # [1,2,3]
+        current_groups = user.groups.all().values_list("id", flat=True)
+        groups_ids_set = set(groups_ids)
+        current_groups_set = set(current_groups) - subs_groups_set
+        final_group_ids = list(groups_ids_set | current_groups_set)
+        user.groups.set(final_group_ids)
 
 
 post_save.connect(user_sub_post_save, UserSubscription)
